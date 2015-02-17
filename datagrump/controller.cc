ead5d96832589450f09bbceff7ac4437085bbcf2
hselin <hselin.chen@gmail.com>
2015-02-17 02:37:43 -0800

    5

--- datagrump/controller.cc
+++ datagrump/controller.cc
@@ -13,16 +13,16 @@ Controller::Controller( const bool debug )
 #define MAX(x, y) ((x > y) ? (x) : (y))
 #define MIN(x, y) ((x < y) ? (x) : (y))
 
-#define TARGET_MAX_LATENCY      (70)
-#define alpha                   (0.1)
+#define TARGET_MAX_LATENCY      (80.0f)
+#define BW_SMOOTHING_ALPHA      (0.05f)
 
 
 /* Get current window size, in datagrams */
 unsigned int Controller::window_size( void )
 {
-  //rintf("this->window_size_: %d\n", this->window_size_);
+  printf("this->window_size_: %d\n", this->window_size_);
 
-
+  //return 25;
   return this->window_size_;
 }
 
@@ -50,14 +50,16 @@ void Controller::datagram_was_sent( const uint64_t sequence_number,
 {
   /* Default: take no action */
 
-  if ( debug_ ) {
+  if ( 0 ) {
     cerr << "At time " << send_timestamp
 	 << " sent datagram " << sequence_number << endl;
   }
+
+  this->last_sent_packet_sequence_number_ = sequence_number;
 }
 
 
-
+#define DIV_ROUND_UP(n,d) (((n) + (d) - 1) / (d))
 
 /* An ack was received */
 void Controller::ack_received( const uint64_t sequence_number_acked,
@@ -66,48 +68,132 @@ void Controller::ack_received( const uint64_t sequence_number_acked,
 			       /* when the acknowledged datagram was sent (sender's clock) */
 			       const uint64_t recv_timestamp_acked,
 			       /* when the acknowledged datagram was received (receiver's clock)*/
-			       const uint64_t timestamp_ack_received )
+			       const uint64_t timestamp_ack_received,
                                /* when the ack was received (by sender) */
+             const uint64_t ack_payload_length)
 {
   /* Default: take no action */
 
-  if ( debug_ ) {
+  if ( 0 ) {
     cerr << "At time " << timestamp_ack_received
 	 << " received ack for datagram " << sequence_number_acked
 	 << " (send @ time " << send_timestamp_acked
 	 << ", received @ time " << recv_timestamp_acked << " by receiver's clock)"
+   << " with payload "<< ack_payload_length
 	 << endl;
   }
 
   //this->most_recent_rtt_ = (timestamp_ack_received - send_timestamp_acked);
 
-  uint64_t diff  = 0;
+  uint64_t elapsed_time = 0;
+  float sample_bw = 0;
+  uint64_t target_amount_of_outstanding_data = 0;
+  uint64_t target_amount_of_outstanding_packets = 0;
+  uint64_t num_outstanding_packets = 0;
+  uint64_t target_window_size = 0;
+
 
   if(this->prev_recv_timestamp_acked_)
   {
-    diff = recv_timestamp_acked - this->prev_recv_timestamp_acked_;
+    elapsed_time = recv_timestamp_acked - this->prev_recv_timestamp_acked_;
   }
 
   this->prev_recv_timestamp_acked_ = recv_timestamp_acked;
-  
 
-  if(diff < TARGET_MAX_LATENCY)
+  if(elapsed_time == 0)
   {
-    this->window_size_ = (unsigned int) MIN(this->window_size_ + 2, 20);
+    sample_bw = 500.00f;
   }
   else
   {
-    this->window_size_ = (unsigned int) MAX((int)(this->window_size_ - 5), 2);
+    sample_bw = ((float) ack_payload_length / (float)elapsed_time);
   }
 
+  this->estimated_bw_ = (BW_SMOOTHING_ALPHA * this->estimated_bw_) + ((1.0f - BW_SMOOTHING_ALPHA) * sample_bw);
 
-  if(1)
+  //printf("ack_payload_length = %lu elapsed_time = %f\n", ack_payload_length, elapsed_time);
+
+
+
+
+  printf("bw [%f, %f]\n", sample_bw, this->estimated_bw_);
+
+
+  target_amount_of_outstanding_data = (TARGET_MAX_LATENCY * this->estimated_bw_);
+
+  
+  //printf("target_amount_of_outstanding_data [%lu]\n", target_amount_of_outstanding_data);
+
+  target_amount_of_outstanding_packets = DIV_ROUND_UP(target_amount_of_outstanding_data, ack_payload_length);
+
+  //printf("target_amount_of_outstanding_packets [%lu]\n", target_amount_of_outstanding_packets);
+
+  /*
+  if(diff < TARGET_MAX_LATENCY)
   {
-    printf("this->most_recent_rtt_: %lu\n", diff);
+    this->window_size_ = (unsigned int) MIN(this->window_size_ + 1, 40);
+  }
+  else
+  {
+    this->window_size_ = (unsigned int) MAX((int)(this->window_size_ - 5), 5);
+  }
+  */
+
+  //target_amount_of_outstanding_packets = 25;
+
+
+
+  //this->window_size_ = target_amount_of_outstanding_packets;
+
+  num_outstanding_packets = this->last_sent_packet_sequence_number_ - sequence_number_acked;
+
+  //printf("[%lu, %lu]\n", this->last_sent_packet_sequence_number_, sequence_number_acked);
+
+
+  //printf("num_outstanding_packets: %lu\n", num_outstanding_packets);
+
+  //printf("[%lu, %lu]\n", num_outstanding_packets, target_amount_of_outstanding_packets);
+
+
+  if(num_outstanding_packets >= target_amount_of_outstanding_packets)
+  {
+    target_window_size = 2;
+  }
+  else
+  {
+    target_window_size = target_amount_of_outstanding_packets - num_outstanding_packets;
+  }
+
+#if 0
+  if(this->window_size_ > target_window_size)
+  {
+    printf("DEC: %lu\n", ((this->window_size_ - target_window_size) / 2));
+    this->window_size_ -= ((this->window_size_ - target_window_size) / 2);
+  }
+  else
+  {
+    printf("INC: %lu\n", ((target_window_size - this->window_size_) / 5));
+    this->window_size_ += ((target_window_size - this->window_size_) / 5);
+  }
+#endif
+
+#if 0
+  if(this->window_size_ < 2)
+    this->window_size_ = 2;
+  
+  if(this->window_size_ >= 20)
+    this->window_size_ = 20;
+#endif
+
+  this->window_size_ = target_window_size;
+
+  if(0)
+  {
+    //printf("this->most_recent_rtt_: %lu\n", diff);
   }
 
 
-  if( 0)
+  if(0)
   {
     printf("this->window_size_: %u\n", this->window_size_);
   }
--- datagrump/controller.hh
+++ datagrump/controller.hh
@@ -9,7 +9,7 @@
 #define INITIAL_WINDOW_SIZE (5)
 #endif
 
-#define INITIAL_WINDOW_SIZE (5)
+#define INITIAL_WINDOW_SIZE (100)
 #define MAX_WINDOW_SIZE (21)
 
 class Controller
@@ -21,8 +21,12 @@ private:
   unsigned int window_size_ = INITIAL_WINDOW_SIZE;
   unsigned int max_window_size_ = MAX_WINDOW_SIZE;
 
-
+  uint64_t last_sent_packet_sequence_number_ = 0;
   uint64_t prev_recv_timestamp_acked_ = 0;
+  uint64_t prev_recv_sequence_number_ = 0;
+  float estimated_bw_ = 0;
+
+
 
   uint64_t most_recent_rtt_ = 0;
   uint64_t ema_rtt_ = 0; 
@@ -46,7 +50,8 @@ public:
   void ack_received( const uint64_t sequence_number_acked,
 		     const uint64_t send_timestamp_acked,
 		     const uint64_t recv_timestamp_acked,
-		     const uint64_t timestamp_ack_received );
+		     const uint64_t timestamp_ack_received,
+         const uint64_t ack_payload_length);
 
   /* How long to wait (in milliseconds) if there are no acks
      before sending one more datagram */
